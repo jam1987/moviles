21b2575ec99f7549c736322b63aefbb37824b6d0
Julio <jam1987@gmail.com>
2014-01-11 00:23:37 -0430

    Primera fase de myGps:Creacion de la App y Anadido el template hecho previamente en diciembre

---
+++ proy2/myGps/www/js/apu.js
@@ -0,0 +1,40 @@
+var apu = (function() {
+     var mapaConfig = {
+               esquema_mapa_ancla: {
+                   chat: {abierto:true, cerrado:true}
+               }};
+    var mapaEstado = {
+                   $contenedor: null,
+                   mapa_ancla : {},
+                   chat_retraido: true
+               };
+    var initModule = function($container) {
+        $container.html(
+             
+             '<div class="apu-encab">'
+               + '<div class="apu-encab-logo"></div>'
+               + '<div class="apu-encab-login"></div>'
+               + '<div class="apu-encab-busqueda"></div>'
+           + '</div>'
+           + '<div class="apu-cuerpo">'
+               + '<div class="apu-cuerpo-nav"></div>'
+               + '<div class="apu-cuerpo-contenido"></div>'
+           + '</div>'
+           + '<div class="apu-pie"></div>'
+           + '<div class="apu-chat"></div>'
+           + '<div class="apu-modal"></div>'
+          
+            );
+            $.uriAnchor.configModule({
+              schema_map : mapaConfig.esquema_mapa_ancla
+           });
+          
+           
+           
+           var chat =  deslizarChat;
+           
+           chat.iniciar(jQuery('#apu'));
+    };
+     var estado = $.uriAnchor.makeAnchorMap();
+    return {initModule: initModule, estado:estado};
+}());
